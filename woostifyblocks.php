c8eb108d82f36040fc05615c6b29e993b941d05e
ngothaiduong2311@gmail.com <>
2023-06-14 21:09:42 +0700

    update version to 0.1.1-beta1

--- woostifyblocks.php
+++ woostifyblocks.php
@@ -5,7 +5,7 @@
  * Description:       WoostifyBlocks offers an extensive library of customizable blocks that seamlessly integrate with the Gutenberg editor
  * Requires at least: 5.8
  * Requires PHP:      7.3
- * Version:           0.1.0-beta1
+ * Version:           0.1.1-beta1
  * Author:            Woostify
  * Author URI:        https://woostify.com/about/
  * License:           GPL-2.0-or-later
@@ -19,13 +19,13 @@
 defined( 'ABSPATH' ) || exit;
 
 // Define Constants.
-define('WCB_UNIQUE_NAME', 'wcb-woostify-blocks-unique');
-define('WCB_VERSION', '0.1.0-beta1');
-define('WCB_FILE', __FILE__);
-define('WCB_PLUGIN_BASE', plugin_basename(WCB_FILE));
-define('WCB_PATH', plugin_dir_path(WCB_FILE));
-define('WCB_URI', plugins_url('/', WCB_FILE));
-define('WCB_BUILD_PATH', __DIR__ . '/build');
+define( 'WCB_UNIQUE_NAME', 'wcb-woostify-blocks-unique' );
+define( 'WCB_VERSION', '0.1.1-beta1' );
+define( 'WCB_FILE', __FILE__ );
+define( 'WCB_PLUGIN_BASE', plugin_basename( WCB_FILE ) );
+define( 'WCB_PATH', plugin_dir_path( WCB_FILE ) );
+define( 'WCB_URI', plugins_url( '/', WCB_FILE ) );
+define( 'WCB_BUILD_PATH', __DIR__ . '/build' );
 
 
 require plugin_dir_path( __FILE__ ) . 'includes/wcb-custom-funcs.php';
