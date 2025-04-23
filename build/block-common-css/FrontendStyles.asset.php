4bd8ad2e919471dabf9d83fda3470cbffbbbaeab
vietpro123 <viet09cntt@gmail.com>
2025-04-23 20:30:29 +0700

    add toggle open new windown

--- build/block-common-css/FrontendStyles.asset.php
+++ build/block-common-css/FrontendStyles.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api', 'wp-element'), 'version' => '747479f3fcfb960e4684');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api'), 'version' => '11697d376383338d6d39');
