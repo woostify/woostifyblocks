ed39848fca480cc67bebebbbccabb6239ad248de
vietpro123 <viet09cntt@gmail.com>
2025-05-06 22:44:38 +0700

    add file build

---
+++ build/src_block-map_GlobalCss_tsx.js
@@ -0,0 +1,516 @@
+"use strict";
+(globalThis["webpackChunkwoostifyblocks"] = globalThis["webpackChunkwoostifyblocks"] || []).push([["src_block-map_GlobalCss_tsx"],{
+
+/***/ "./src/block-container/getAdvanveStyles.ts":
+/*!*************************************************!*\
+  !*** ./src/block-container/getAdvanveStyles.ts ***!
+  \*************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   getAdvanveDivWrapStyles: () => (/* binding */ getAdvanveDivWrapStyles)
+/* harmony export */ });
+/* harmony import */ var _emotion_react__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @emotion/react */ "./node_modules/@emotion/react/dist/emotion-react.browser.esm.js");
+/* harmony import */ var ___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../________ */ "./src/________.ts");
+/* harmony import */ var _utils_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../utils/checkResponsiveValueForOptimizeCSS */ "./src/utils/checkResponsiveValueForOptimizeCSS.ts");
+
+
+
+const getAdvanveDivWrapStyles = ({
+  advance_motionEffect,
+  advance_zIndex,
+  advance_responsiveCondition,
+  className,
+  defaultDisplay
+}) => {
+  const {
+    media_desktop,
+    media_tablet
+  } = ___WEBPACK_IMPORTED_MODULE_0__.DEMO_WCB_GLOBAL_VARIABLES;
+  //
+  //
+  try {
+    const thisELs = document.querySelectorAll(className);
+    if (advance_motionEffect && advance_motionEffect.entranceAnimation && thisELs && thisELs.length) {
+      console.log(222, {
+        thisELs
+      });
+      thisELs.forEach(element => {
+        // remove old class
+        const regex = /\banimate__\S+/g;
+        const classRemoved = element?.className.replace(regex, "");
+        element.setAttribute("class", classRemoved);
+
+        // add new class
+        setTimeout(() => {
+          element?.classList.add("animate__animated", `animate__${advance_motionEffect?.entranceAnimation}`, `animate__${advance_motionEffect?.animationDuration}`, `animate__delay-${advance_motionEffect?.animationDelay}ms`, `animate__repeat-${advance_motionEffect?.repeat}`);
+        }, 50);
+      });
+    }
+  } catch (error) {
+    console.log(123, "error, advance_motionEffect", error);
+  }
+  const {
+    mobile_v: zIndexMobile,
+    tablet_v: zIndexTablet,
+    desktop_v: zIndexDesktop
+  } = (0,_utils_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_1__["default"])({
+    mobile_v: advance_zIndex.Mobile || advance_zIndex.Tablet || advance_zIndex.Desktop,
+    tablet_v: advance_zIndex.Tablet || advance_zIndex.Desktop,
+    desktop_v: advance_zIndex.Desktop
+  });
+  //
+
+  const {
+    mobile_v: isHiddenOnMobile,
+    tablet_v: isHiddenOnTablet,
+    desktop_v: isHiddenOnDesktop
+  } = (0,_utils_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_1__["default"])({
+    mobile_v: advance_responsiveCondition.isHiddenOnMobile,
+    tablet_v: advance_responsiveCondition.isHiddenOnTablet,
+    desktop_v: advance_responsiveCondition.isHiddenOnDesktop
+  });
+  return (0,_emotion_react__WEBPACK_IMPORTED_MODULE_2__.css)`
+		${className} {
+			display: ${isHiddenOnMobile ? "none" : defaultDisplay};
+			visibility: visible;
+			z-index: ${zIndexMobile};
+			@media (min-width: ${media_tablet}) {
+				z-index: ${zIndexTablet};
+				display: ${isHiddenOnTablet !== "" ? isHiddenOnTablet ? "none" : defaultDisplay : ""};
+			}
+			@media (min-width: ${media_desktop}) {
+				z-index: ${zIndexDesktop};
+				display: ${isHiddenOnDesktop !== "" ? isHiddenOnDesktop ? "none" : defaultDisplay : ""};
+			}
+		}
+	`;
+};
+
+/***/ }),
+
+/***/ "./src/block-map/GlobalCss.tsx":
+/*!*************************************!*\
+  !*** ./src/block-map/GlobalCss.tsx ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _emotion_react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @emotion/react */ "./node_modules/@emotion/react/dist/emotion-react.browser.esm.js");
+/* harmony import */ var _block_container_getAdvanveStyles__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../block-container/getAdvanveStyles */ "./src/block-container/getAdvanveStyles.ts");
+/* harmony import */ var _utils_getBorderStyles__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../utils/getBorderStyles */ "./src/utils/getBorderStyles.ts");
+/* harmony import */ var _utils_getStyleObjectFromResponsiveAttr__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../utils/getStyleObjectFromResponsiveAttr */ "./src/utils/getStyleObjectFromResponsiveAttr.ts");
+/* harmony import */ var ___WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../________ */ "./src/________.ts");
+
+
+
+
+
+
+
+const GlobalCss = attrs => {
+  const {
+    uniqueId,
+    // ATTRS OF BLOCK
+    general_general,
+    style_border,
+    //
+    advance_responsiveCondition,
+    advance_zIndex,
+    advance_motionEffect
+  } = attrs;
+  const {
+    media_desktop,
+    media_tablet
+  } = ___WEBPACK_IMPORTED_MODULE_4__.DEMO_WCB_GLOBAL_VARIABLES;
+  const WRAP_CLASSNAME = `.${uniqueId}[data-uniqueid=${uniqueId}]`;
+  const INNER_CLASSNAME = `${WRAP_CLASSNAME} .wcb-map__inner`;
+
+  // ------------------- WRAP DIV
+  const getDivWrapStyles = () => {
+    return {
+      [`${WRAP_CLASSNAME}`]: {
+        [`@media (min-width: ${media_tablet})`]: {},
+        [`@media (min-width: ${media_desktop})`]: {}
+      }
+    };
+  };
+  if (!uniqueId) {
+    return null;
+  }
+  return (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(react__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(_emotion_react__WEBPACK_IMPORTED_MODULE_5__.Global, {
+    styles: [(0,_utils_getBorderStyles__WEBPACK_IMPORTED_MODULE_2__["default"])({
+      className: WRAP_CLASSNAME,
+      border: style_border,
+      isWithRadius: true,
+      isWithIframe: true
+    }), (0,_utils_getStyleObjectFromResponsiveAttr__WEBPACK_IMPORTED_MODULE_3__["default"])({
+      className: INNER_CLASSNAME,
+      value: general_general.height,
+      prefix: "height"
+    })]
+  }), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(_emotion_react__WEBPACK_IMPORTED_MODULE_5__.Global, {
+    styles: (0,_block_container_getAdvanveStyles__WEBPACK_IMPORTED_MODULE_1__.getAdvanveDivWrapStyles)({
+      advance_motionEffect,
+      advance_responsiveCondition,
+      advance_zIndex,
+      className: WRAP_CLASSNAME,
+      defaultDisplay: "block"
+    })
+  }));
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (react__WEBPACK_IMPORTED_MODULE_0___default().memo(GlobalCss));
+
+/***/ }),
+
+/***/ "./src/utils/checkResponsiveValueForOptimizeCSS.ts":
+/*!*********************************************************!*\
+  !*** ./src/utils/checkResponsiveValueForOptimizeCSS.ts ***!
+  \*********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+const checkResponsiveValueForOptimizeCSS = ({
+  mobile_v = null,
+  tablet_v = null,
+  desktop_v = null
+}) => {
+  let new_tablet_v = tablet_v;
+  let new_desktop_v = desktop_v;
+  if (mobile_v === tablet_v && tablet_v === desktop_v) {
+    return {
+      mobile_v,
+      tablet_v: null,
+      desktop_v: null
+    };
+  }
+  if (desktop_v === tablet_v || desktop_v === mobile_v) {
+    new_desktop_v = null;
+  }
+  if (tablet_v === mobile_v) {
+    new_tablet_v = null;
+  }
+  return {
+    mobile_v: mobile_v !== null && mobile_v !== void 0 ? mobile_v : null,
+    tablet_v: new_tablet_v !== null && new_tablet_v !== void 0 ? new_tablet_v : null,
+    desktop_v: new_desktop_v !== null && new_desktop_v !== void 0 ? new_desktop_v : null
+  };
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (checkResponsiveValueForOptimizeCSS);
+
+/***/ }),
+
+/***/ "./src/utils/getBorderRadiusStyles.ts":
+/*!********************************************!*\
+  !*** ./src/utils/getBorderRadiusStyles.ts ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var ___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../________ */ "./src/________.ts");
+/* harmony import */ var _getValueFromAttrsResponsives__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./getValueFromAttrsResponsives */ "./src/utils/getValueFromAttrsResponsives.ts");
+/* harmony import */ var _checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./checkResponsiveValueForOptimizeCSS */ "./src/utils/checkResponsiveValueForOptimizeCSS.ts");
+
+
+
+const getBorderRadiusStyles = ({
+  className,
+  radius,
+  isWithIframe = false
+}) => {
+  const {
+    media_desktop,
+    media_tablet
+  } = ___WEBPACK_IMPORTED_MODULE_0__.DEMO_WCB_GLOBAL_VARIABLES;
+  let {
+    value_Desktop: radiusDesktop,
+    value_Tablet: radiusTablet,
+    value_Mobile: radiusMobile
+  } = (0,_getValueFromAttrsResponsives__WEBPACK_IMPORTED_MODULE_1__["default"])(radius);
+  const converttted = radiusValue => {
+    let newradiusValue = radiusValue;
+    if (typeof radiusValue === "string") {
+      newradiusValue = {
+        bottomLeft: radiusValue,
+        bottomRight: radiusValue,
+        topLeft: radiusValue,
+        topRight: radiusValue
+      };
+    } else {
+      newradiusValue = {
+        bottomLeft: radiusValue?.bottomLeft,
+        bottomRight: radiusValue?.bottomRight,
+        topLeft: radiusValue?.topLeft,
+        topRight: radiusValue?.topRight
+      };
+    }
+    return newradiusValue;
+  };
+  radiusDesktop = converttted(radiusDesktop);
+  radiusTablet = converttted(radiusTablet);
+  radiusMobile = converttted(radiusMobile);
+  const {
+    mobile_v: mobile_v_topLeft,
+    tablet_v: tablet_v_topLeft,
+    desktop_v: desktop_v_topLeft
+  } = (0,_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_2__["default"])({
+    mobile_v: radiusMobile?.topLeft,
+    tablet_v: radiusTablet?.topLeft,
+    desktop_v: radiusDesktop?.topLeft
+  });
+  const {
+    mobile_v: mobile_v_topRight,
+    tablet_v: tablet_v_topRight,
+    desktop_v: desktop_v_topRight
+  } = (0,_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_2__["default"])({
+    mobile_v: radiusMobile?.topRight,
+    tablet_v: radiusTablet?.topRight,
+    desktop_v: radiusDesktop?.topRight
+  });
+  const {
+    mobile_v: mobile_v_bottomRight,
+    tablet_v: tablet_v_bottomRight,
+    desktop_v: desktop_v_bottomRight
+  } = (0,_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_2__["default"])({
+    mobile_v: radiusMobile?.bottomRight,
+    tablet_v: radiusTablet?.bottomRight,
+    desktop_v: radiusDesktop?.bottomRight
+  });
+  const {
+    mobile_v: mobile_v_bottomLeft,
+    tablet_v: tablet_v_bottomLeft,
+    desktop_v: desktop_v_bottomLeft
+  } = (0,_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_2__["default"])({
+    mobile_v: radiusMobile?.bottomLeft,
+    tablet_v: radiusTablet?.bottomLeft,
+    desktop_v: radiusDesktop?.bottomLeft
+  });
+
+  // Check if className is for iframe
+  const applyImportant = isWithIframe ? "!important" : "";
+  return {
+    [`${className}`]: {
+      borderTopLeftRadius: `${mobile_v_topLeft} ${applyImportant}`,
+      borderTopRightRadius: `${mobile_v_topRight} ${applyImportant}`,
+      borderBottomRightRadius: `${mobile_v_bottomRight} ${applyImportant}`,
+      borderBottomLeftRadius: `${mobile_v_bottomLeft} ${applyImportant}`,
+      [`@media (min-width: ${media_tablet})`]: tablet_v_topLeft || tablet_v_topRight || tablet_v_bottomRight || tablet_v_bottomLeft ? {
+        borderTopLeftRadius: `${tablet_v_topLeft} ${applyImportant}`,
+        borderTopRightRadius: `${tablet_v_topRight} ${applyImportant}`,
+        borderBottomRightRadius: `${tablet_v_bottomRight} ${applyImportant}`,
+        borderBottomLeftRadius: `${tablet_v_bottomLeft} ${applyImportant}`
+      } : null,
+      [`@media (min-width: ${media_desktop})`]: desktop_v_topLeft || desktop_v_topRight || desktop_v_bottomRight || desktop_v_bottomLeft ? {
+        borderTopLeftRadius: `${desktop_v_topLeft} ${applyImportant}`,
+        borderTopRightRadius: `${desktop_v_topRight} ${applyImportant}`,
+        borderBottomRightRadius: `${desktop_v_bottomRight} ${applyImportant}`,
+        borderBottomLeftRadius: `${desktop_v_bottomLeft} ${applyImportant}`
+      } : null
+    }
+  };
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getBorderRadiusStyles);
+
+/***/ }),
+
+/***/ "./src/utils/getBorderStyles.ts":
+/*!**************************************!*\
+  !*** ./src/utils/getBorderStyles.ts ***!
+  \**************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var _getBorderRadiusStyles__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./getBorderRadiusStyles */ "./src/utils/getBorderRadiusStyles.ts");
+
+const getBorderStyles = ({
+  border,
+  className,
+  isWithRadius = false,
+  isWithIframe = false
+}) => {
+  const {
+    hoverColor,
+    mainSettings,
+    radius
+  } = border;
+  //
+
+  // MAIN BORDER
+  let CSSObject = {
+    [`${className}`]: {}
+  };
+  if (mainSettings) {
+    if (mainSettings?.top) {
+      const {
+        bottom,
+        left,
+        right,
+        top
+      } = mainSettings;
+      CSSObject = {
+        [`${className}`]: {
+          borderTop: `${top.width} ${top.style || "none"} ${top.color || ""}`,
+          borderLeft: `${left.width} ${left.style || "none"} ${left.color || ""}`,
+          borderRight: `${right.width} ${right.style || "none"} ${right.color || ""}`,
+          borderBottom: `${bottom.width} ${bottom.style || "none"} ${bottom.color || ""}`,
+          "&:hover": {
+            borderColor: `${hoverColor}`
+          }
+        }
+      };
+    } else {
+      const {
+        color,
+        style,
+        width
+      } = mainSettings;
+      CSSObject = {
+        [`${className}`]: {
+          border: `${width} ${style || "none"} ${color || ""}`,
+          "&:hover": {
+            borderColor: `${hoverColor || ""}`
+          }
+        }
+      };
+    }
+  }
+
+  // RAIDUS
+  let radiusCSSObject = {
+    [`${className}`]: {}
+  };
+  if (isWithRadius && radius) {
+    radiusCSSObject = (0,_getBorderRadiusStyles__WEBPACK_IMPORTED_MODULE_0__["default"])({
+      radius,
+      className,
+      isWithIframe
+    });
+  }
+
+  //
+  let a = {};
+  let b = {};
+  if (typeof CSSObject[className] === "object") {
+    a = CSSObject[className] || {};
+  }
+  if (typeof radiusCSSObject[className] === "object") {
+    b = radiusCSSObject[className] || {};
+  }
+  return {
+    [`${className}`]: {
+      ...a,
+      ...b
+    }
+  };
+};
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getBorderStyles);
+
+/***/ }),
+
+/***/ "./src/utils/getStyleObjectFromResponsiveAttr.ts":
+/*!*******************************************************!*\
+  !*** ./src/utils/getStyleObjectFromResponsiveAttr.ts ***!
+  \*******************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var ___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../________ */ "./src/________.ts");
+/* harmony import */ var _getValueFromAttrsResponsives__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./getValueFromAttrsResponsives */ "./src/utils/getValueFromAttrsResponsives.ts");
+/* harmony import */ var _checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./checkResponsiveValueForOptimizeCSS */ "./src/utils/checkResponsiveValueForOptimizeCSS.ts");
+
+
+
+function getStyleObjectFromResponsiveAttr({
+  className,
+  prefix,
+  prefix_2,
+  prefix_3,
+  prefix_4,
+  value,
+  hasUnit = true,
+  unit
+}) {
+  const {
+    media_desktop,
+    media_tablet
+  } = ___WEBPACK_IMPORTED_MODULE_0__.DEMO_WCB_GLOBAL_VARIABLES;
+  let {
+    value_Desktop,
+    value_Tablet,
+    value_Mobile
+  } = (0,_getValueFromAttrsResponsives__WEBPACK_IMPORTED_MODULE_1__["default"])(value);
+  if (!hasUnit && !!unit) {
+    if (typeof value_Desktop === "number") {
+      value_Desktop = value_Desktop + unit;
+      value_Tablet = value_Tablet + unit;
+      value_Mobile = value_Mobile + unit;
+    }
+    if (typeof value_Desktop === "string") {
+      value_Desktop = value_Desktop ? value_Desktop + unit : null;
+      value_Tablet = value_Tablet ? value_Tablet + unit : null;
+      value_Mobile = value_Mobile ? value_Mobile + unit : null;
+    }
+  }
+
+  //
+  let prefix2 = prefix_2 || "";
+  let prefix3 = prefix_3 || "";
+  let prefix4 = prefix_4 || "";
+
+  //
+  const {
+    mobile_v: value_Mobile_new,
+    tablet_v: value_Tablet_new,
+    desktop_v: value_Desktop_new
+  } = (0,_checkResponsiveValueForOptimizeCSS__WEBPACK_IMPORTED_MODULE_2__["default"])({
+    mobile_v: value_Mobile,
+    tablet_v: value_Tablet,
+    desktop_v: value_Desktop
+  });
+  //
+
+  return {
+    [className]: {
+      [prefix]: value_Mobile_new !== null && value_Mobile_new !== void 0 ? value_Mobile_new : null,
+      [prefix2]: prefix_2 ? value_Mobile_new !== null && value_Mobile_new !== void 0 ? value_Mobile_new : null : null,
+      [prefix3]: prefix_3 ? value_Mobile_new !== null && value_Mobile_new !== void 0 ? value_Mobile_new : null : null,
+      [prefix4]: prefix_4 ? value_Mobile_new !== null && value_Mobile_new !== void 0 ? value_Mobile_new : null : null,
+      [`@media (min-width: ${media_tablet})`]: value_Tablet_new ? {
+        [prefix]: value_Tablet_new,
+        [prefix2]: prefix_2 ? value_Tablet_new : null,
+        [prefix3]: prefix_3 ? value_Tablet_new : null,
+        [prefix4]: prefix_4 ? value_Tablet_new : null
+      } : undefined,
+      [`@media (min-width: ${media_desktop})`]: value_Desktop_new ? {
+        [prefix]: value_Desktop_new,
+        [prefix2]: prefix_2 ? value_Desktop_new : null,
+        [prefix3]: prefix_3 ? value_Desktop_new : null,
+        [prefix4]: prefix_4 ? value_Desktop_new : null
+      } : undefined
+    }
+  };
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (getStyleObjectFromResponsiveAttr);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=src_block-map_GlobalCss_tsx.js.map
