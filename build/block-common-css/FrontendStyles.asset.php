e1867aea5115b303629f02fe843d3722517132e7
vietpro123 <viet09cntt@gmail.com>
2025-05-27 23:50:04 +0700

    fix: display icon box in preview screen

--- build/block-common-css/FrontendStyles.asset.php
+++ build/block-common-css/FrontendStyles.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api'), 'version' => 'a82173632ec4cbaca684');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api'), 'version' => 'cf16568c2bab71dcc4d1');
