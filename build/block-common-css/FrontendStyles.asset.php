468510814b447b3f7c0a9ae36298979f0f12acde
vietpro123 <viet09cntt@gmail.com>
2025-05-27 23:19:55 +0700

    re-build

--- build/block-common-css/FrontendStyles.asset.php
+++ build/block-common-css/FrontendStyles.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api'), 'version' => '225dcd671e047d86ceaa');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'react-dom', 'wp-api'), 'version' => '48dbca053fc7e0e6ccb7');
--- build/block-icon-list/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9d00352b43339fd1dc9e');
