c6cfadd739b0210e425c72b99a306b3922217422
vietpro123 <viet09cntt@gmail.com>
2025-06-01 11:31:59 +0700

    remove block icon list backup

--- build/block-icon-list-backup/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '315074b630f247fffb17');
