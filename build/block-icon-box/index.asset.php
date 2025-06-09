6f22b627eab009278cd0afa8b826ac70eb81eae3
vietpro123 <viet09cntt@gmail.com>
2025-06-09 22:23:01 +0700

    fix: set option border radius with default Tablet, Mobile when set value for desktop and set each Table, Mobile

--- build/block-icon-box/index.asset.php
+++ build/block-icon-box/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8c7e5ab8f17990f03093');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4f3d7c1fbb4ebce5bda4');
