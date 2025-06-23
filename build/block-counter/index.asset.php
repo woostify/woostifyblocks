ac3488265231dd1180d7929dd97b4fc9c9468c6d
vietpro123 <viet09cntt@gmail.com>
2025-06-23 23:14:30 +0700

    fix load note wotify

--- build/block-counter/index.asset.php
+++ build/block-counter/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '002bfc69ade13c0ed76c');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'b07d63acdf8132350cd5');
