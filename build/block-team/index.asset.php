8ff2051b7fe51f3892f8deaea4e6fc8b57363b69
vietpro123 <viet09cntt@gmail.com>
2025-06-23 23:22:05 +0700

    remove slider outdate

--- build/block-team/index.asset.php
+++ build/block-team/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1612c8c1fb5bf022c496');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '88ea8ac2ffc875599d9e');
