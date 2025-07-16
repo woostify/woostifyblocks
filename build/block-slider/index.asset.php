305108957fc817de38a6949a67ecaba2478b6fef
vietpro123 <viet09cntt@gmail.com>
2025-07-16 23:49:49 +0700

    add file build

--- build/block-slider/index.asset.php
+++ build/block-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '4d9e040579c08719afbb');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '40b4e40125d73ef0b09e');
