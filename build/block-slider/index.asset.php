eab3d1c961f1194b1ac00121d66d8ab8b10ed5c7
vietpro123 <viet09cntt@gmail.com>
2025-07-23 00:30:55 +0700

    slider style

--- build/block-slider/index.asset.php
+++ build/block-slider/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f5c409e652e58d75cbf2');
+<?php return array('dependencies' => array('jquery', 'lodash', 'react', 'wp-api', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '219c7fa180c31ce5a890');
