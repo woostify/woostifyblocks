39dd56e96719bf897702ba980676e07dd5a40b54
DESKTOP-D6OA4BT\V-PC <viet09cntt@gmail.com>
2025-08-17 23:31:32 +0700

    update block map

--- build/block-tab-child/index.asset.php
+++ build/block-tab-child/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data'), 'version' => 'aea7f5fd6fb299ddc902');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-data'), 'version' => '2c62ead7b337601308e7');
